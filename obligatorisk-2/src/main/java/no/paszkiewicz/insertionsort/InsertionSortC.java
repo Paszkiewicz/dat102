c4fb2ab9056fa28e0847120661945a83bd8178c6
larsbirger <larsbirger.bergmaal@gmail.com>
2026-02-20 20:59:12 +0100

    Use sentinel and pair-wise insertion sort
    Refactor insertionSort to use a sentinel and pair-wise insertion. Moves the smallest element to the front (sentinel), sorts adjacent pairs locally, then inserts the larger then the smaller of each pair into the sorted prefix. Adds early return for arrays with length < 2 and handles the last element when the array length is odd. Cleans up variables and comments and fixes the comparison direction in the final insertion loop.

--- obligatorisk-2/src/main/java/no/paszkiewicz/insertionsort/InsertionSortC.java
+++ obligatorisk-2/src/main/java/no/paszkiewicz/insertionsort/InsertionSortC.java
@@ -5,58 +5,55 @@ import java.util.Random;
 public class InsertionSortC {
 
     public static void insertionSort(int[] array) {
-
-        for (int i = array.length - 1; i > 0; i--) {
+        int n = array.length;
+        if (n < 2) return;
+    
+        // 1. Flytt minste element fremst (Vaktpost/Sentinel)
+        for (int i = n - 1; i > 0; i--) {
             if (array[i] < array[i - 1]) {
                 int temp = array[i];
                 array[i] = array[i - 1];
                 array[i - 1] = temp;
             }
         }
-        
-        int tall = 1;
-
-        for (int i = tall; i < array.length - 1; i += 2) {
+    
+        // 2. Sorter to og to (Oppgave 1b/c)
+        int i = 1; 
+        for (; i <= n - 2; i += 2) { // Kjører så lenge vi har et par tilgjengelig
             int v1 = array[i];
             int v2 = array[i + 1];
-            
-            if (v1 > v2) { 
-                int t = v1;
-                v1 = v2;
-                v2 = t;
+    
+            if (v1 > v2) {
+                int t = v1; v1 = v2; v2 = t;
             }
-
+    
             int j = i - 1;
-            
+            // Flytt elementer for å gi plass til v2 (den største av paret)
             while (array[j] > v2) {
                 array[j + 2] = array[j];
                 j--;
             }
-            
             array[j + 2] = v2;
-            
+    
+            // Flytt elementer for å gi plass til v1 (den minste av paret)
             while (array[j] > v1) {
                 array[j + 1] = array[j];
                 j--;
             }
             array[j + 1] = v1;
-
-            tall = i;
         }
-
-        // if (tall < array.length) {
-        //         int key = array[tall];
-        //         int j = tall - 1;
-                
-        //         while (array[j] < key) {
-        //             array[j + 1] = array[j];
-        //             j--;
-        //         }
-                
-        //         array[j + 1] = key;
-        // }
+    
+        // 3. Håndter siste elementet hvis n er oddetall 
+        if (i < n) {
+            int key = array[i];
+            int j = i - 1;
+            while (array[j] > key) { // Endret fra < til >
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }
     }
-
 
     public static void main(String[] args) {
         Random tilfeldig = new Random();
